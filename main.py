9610b3af8173e22d26516ffa7c4d090262767699
Abodrer <v5xr35950@gmail.com>
2024-12-31 20:12:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello world") 
+from telebot import TeleBot
+from handlers import Handlers
+from config import TOKEN
+
+bot = TeleBot(TOKEN)
+
+# إعداد المعالجات
+handlers = Handlers(bot)
+handlers.setup_handlers()
+
+# تشغيل البوت
+if __name__ == "__main__":
+    print("🚀 البوت قيد التشغيل...")
+    bot.polling(non_stop=True)
